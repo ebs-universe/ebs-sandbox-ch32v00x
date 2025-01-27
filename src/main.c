2615d8e3b52fcd9c0e5fc8863a17760ce98c6b7f
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2025-01-27 14:18:23 +0530

    Fix whitespace

--- src/main.c
+++ src/main.c
@@ -12,34 +12,34 @@
 
 
 static void setup_handlers(void) {
-	__core_handler_inclusion = 1;
-	__exti_handler_inclusion = 1;
+    __core_handler_inclusion = 1;
+    __exti_handler_inclusion = 1;
 }
 
 static ucdm_addr_t setup_core(ucdm_addr_t ucdm_address) {
-	setup_handlers();
-	core_init();
-	ucdm_init();
+    setup_handlers();
+    core_init();
+    ucdm_init();
     ucdm_address = tm_init(ucdm_address);
     #if APP_ENABLE_QDELAY
         qdelay_init();
     #endif
-	return ucdm_address;			
+    return ucdm_address;			
 }
 
 static ucdm_addr_t setup_peripherals(ucdm_addr_t ucdm_address) {
     application_gpio_init();
 
-	#if APP_ENABLE_ID 
+    #if APP_ENABLE_ID 
         id_init();
     #endif
-	#if APP_ENABLE_TIME_SYNC
+    #if APP_ENABLE_TIME_SYNC
         setup_time_sync();
     #endif
-	#if APP_ENABLE_OUTPUT_BLINK
+    #if APP_ENABLE_OUTPUT_BLINK
         start_blink_task();
     #endif
-	return ucdm_address;
+    return ucdm_address;
 }
 
 ucdm_addr_t setup_system(ucdm_addr_t ucdm_address) {
@@ -49,19 +49,19 @@ ucdm_addr_t setup_system(ucdm_addr_t ucdm_address) {
 }
 
 static ucdm_addr_t setup_application(ucdm_addr_t ucdm_address) {
-	return ucdm_address;
+    return ucdm_address;
 }
 
 int main(void)
 {
-	ucdm_addr_t ucdm_address = 1;
-	ucdm_address = setup_system(ucdm_address);
-	ucdm_address = setup_application(ucdm_address);
+    ucdm_addr_t ucdm_address = 1;
+    ucdm_address = setup_system(ucdm_address);
+    ucdm_address = setup_application(ucdm_address);
 	
-	while(1)
-	{
-		#if APP_ENABLE_TIME_CRON
-        	tm_cron_poll();
+    while(1)
+    {
+        #if APP_ENABLE_TIME_CRON
+        tm_cron_poll();
         #endif
-	}	
+    }	
 }
